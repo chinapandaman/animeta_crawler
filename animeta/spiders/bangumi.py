b60aa6e2171bdc3ba67ffb707a5a05472c416b7e
chinapandaman <b.j.li@foxmail.com>
2020-02-08 16:10:10 -0600

    black, disable cookies

--- animeta/spiders/bangumi.py
+++ animeta/spiders/bangumi.py
@@ -5,9 +5,7 @@ class BangumiSpider(scrapy.Spider):
     name = "bangumi"
 
     def start_requests(self):
-        urls = [
-            'https://bangumi.tv/anime/browser?sort=rank&page=1',
-        ]
+        urls = ["https://bangumi.tv/anime/browser?sort=rank&page=1"]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
@@ -17,7 +15,9 @@ class BangumiSpider(scrapy.Spider):
                 "title": each.css("a::text").get(),
                 "score": each.css("p.rateInfo").css("small.fade::text").get(),
             }
-            
+
         if len(response.css("div.section").css("div.inner")):
-             next_page = response.urljoin(response.css("div.page_inner").css("a.p::attr(href)")[-2].get())
-             yield scrapy.Request(next_page, callback=self.parse)
+            next_page = response.urljoin(
+                response.css("div.page_inner").css("a.p::attr(href)")[-2].get()
+            )
+            yield scrapy.Request(next_page, callback=self.parse)
