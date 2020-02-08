d36ad2f2acb5b4e07113cd6e47ac9741c366764d
chinapandaman <b.j.li@foxmail.com>
2020-02-08 14:59:22 -0600

    recursive bangumi spider

--- animeta/spiders/bangumi.py
+++ animeta/spiders/bangumi.py
@@ -17,3 +17,7 @@ class BangumiSpider(scrapy.Spider):
                 "title": each.css("a::text").get(),
                 "score": each.css("p.rateInfo").css("small.fade::text").get(),
             }
+            
+        if len(response.css("div.section").css("div.inner")):
+             next_page = response.urljoin(response.css("div.page_inner").css("a.p::attr(href)")[-2].get())
+             yield scrapy.Request(next_page, callback=self.parse)
