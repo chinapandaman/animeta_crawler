25aa5da4ab2bd91f311803578628d7cadf4e90ed
chinapandaman <b.j.li@foxmail.com>
2020-02-08 14:33:40 -0600

    bangumi spider

---
+++ animeta/spiders/bangumi.py
@@ -0,0 +1,19 @@
+import scrapy
+
+
+class BangumiSpider(scrapy.Spider):
+    name = "bangumi"
+
+    def start_requests(self):
+        urls = [
+            'https://bangumi.tv/anime/browser?sort=rank&page=1',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        for each in response.css("div.section").css("div.inner"):
+            yield {
+                "title": each.css("a::text").get(),
+                "score": each.css("p.rateInfo").css("small.fade::text").get(),
+            }
